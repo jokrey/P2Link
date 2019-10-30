0a533757c816060e1a3eb9a79021598d753f075b
jokrey <justin.bennett.developer@gmail.com>
2019-10-30 23:31:52 +0100

    improved java doc

--- src/main/java/jokrey/utilities/network/link2peer/util/P2LTask.java
+++ src/main/java/jokrey/utilities/network/link2peer/util/P2LTask.java
@@ -1,11 +1,20 @@
 package jokrey.utilities.network.link2peer.util;
 
 /**
+ * Task that can be run by the {@link P2LThreadPool}.
+ * Additionally it is a future, i.e. the underlying task produces a result that completes said future.
+ * If the underlying task throws an exception, the future is canceled.
+ *
  * @author jokrey
  */
 public abstract class P2LTask<R> extends P2LFuture<R> {
     private boolean started = false;
-    public void start() {
+    /**
+     * Runs the operation of this task. It produces an arbitrary result that completes the underlying future.
+     * If it throws an exception this tasks underlying future is canceled.
+     * @return the value created by this task
+     */
+    protected void start() {
         started = true;
         try {
             setCompletedOrCanceledBasedOn(run());
@@ -14,11 +23,26 @@ public abstract class P2LTask<R> extends P2LFuture<R> {
             cancel();
         }
     }
+
+    /**
+     * Starts the task on a new thread.
+     * When the task completes it sets this future to complete.
+     */
     public void startOnThread() {
         new Thread(this::start).start();
     }
+
+    /**
+     * @return whether this task is still running
+     */
     public boolean isRunning() {
         return started && !isCanceled() && !isCompleted();
     }
+
+    /**
+     * Abstraction of the operation run by this task. It produces an arbitrary result that completes the underlying future.
+     * If it throws an exception this tasks underlying future is canceled.
+     * @return the value created by this task
+     */
     protected abstract R run();
 }
