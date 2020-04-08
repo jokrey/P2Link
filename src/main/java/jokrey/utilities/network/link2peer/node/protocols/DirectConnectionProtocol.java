342e250b16d393e31aee30eea5d321abbf7cb612
jokrey <justin.bennett.developer@gmail.com>
2020-04-08 20:19:04 +0200

    simplified direct connection protocol

--- src/main/java/jokrey/utilities/network/link2peer/node/protocols/DirectConnectionProtocol.java
+++ src/main/java/jokrey/utilities/network/link2peer/node/protocols/DirectConnectionProtocol.java
@@ -1,6 +1,5 @@
 package jokrey.utilities.network.link2peer.node.protocols;
 
-import jokrey.utilities.bitsandbytes.BitHelper;
 import jokrey.utilities.encoder.as_union.li.bytes.MessageEncoder;
 import jokrey.utilities.network.link2peer.P2LMessage;
 import jokrey.utilities.network.link2peer.P2Link;
@@ -9,12 +8,8 @@ import jokrey.utilities.network.link2peer.node.conversation.P2LConversation;
 import jokrey.utilities.network.link2peer.node.core.P2LConnection;
 import jokrey.utilities.network.link2peer.node.core.P2LNodeInternal;
 import jokrey.utilities.network.link2peer.util.TimeoutException;
-
 import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.security.SecureRandom;
-import java.util.Arrays;
-
 import static jokrey.utilities.network.link2peer.node.core.P2LInternalMessageTypes.SL_DIRECT_CONNECTION_REQUEST;
 import static jokrey.utilities.network.link2peer.node.message_headers.P2LMessageHeader.NO_CONVERSATION_ID;
 import static jokrey.utilities.network.link2peer.node.protocols.RelayedConnectionProtocol.createConversationForInitialDirect;
@@ -23,7 +18,9 @@ import static jokrey.utilities.network.link2peer.node.protocols.RelayedConnectio
  * @author jokrey
  */
 public class DirectConnectionProtocol {
-    private static final SecureRandom secureRandom = new SecureRandom();
+    private static final byte REFUSED = -1;
+    private static final byte ALREADY_CONNECTED = 1;
+    private static final byte NEW_CONNECTION_ESTABLISHED = 2;
 
     public static boolean asInitiator(P2LNodeInternal parent, InetSocketAddress to) {
         return asInitiator(parent, to, NO_CONVERSATION_ID);
@@ -32,33 +29,24 @@ public class DirectConnectionProtocol {
         System.out.println(parent.getSelfLink()+" - DirectConnectionProtocol.asInitiator - to = " + to);
         try {
             if (parent.connectionLimitReached()) return false;
-            System.out.println(parent.getSelfLink()+" - "+1);
 
             short conversationId = conversationIdOverride == NO_CONVERSATION_ID ? createConversationForInitialDirect(parent) : conversationIdOverride;
             P2LConversation convo = parent.internalConvo(SL_DIRECT_CONNECTION_REQUEST, conversationId, to);
 
-            System.out.println(parent.getSelfLink()+" - "+2);
-            byte[] verifyNonce = convo.initExpectData(linkToMessage(parent.getSelfLink(), convo));
-            System.out.println(parent.getSelfLink()+" - verifyNonce = " + Arrays.toString(verifyNonce));
+            ReceivedP2LMessage peerLinkMessage = convo.initExpect(linkToMessage(parent.getSelfLink(), convo));
+            convo.close();
+            byte result = peerLinkMessage.nextByte();
 
-            if (verifyNonce.length == 0) {
-                convo.close();
+            if (result == REFUSED) {
                 return false;
-            } else if (verifyNonce.length == 4) {
-                parent.graduateToEstablishedConnection(new P2LConnection(new P2Link.Direct(to), to, BitHelper.getInt32From(verifyNonce), convo.getAvRTT()), conversationId);
-                convo.close();
-                System.out.println(parent.getSelfLink()+" - asInitiatorDirect success because verify nonce == 4 - [ALREADY CONNECTED]");
+            } else if (result == ALREADY_CONNECTED) {
+                parent.graduateToEstablishedConnection(new P2LConnection(new P2Link.Direct(to), to, peerLinkMessage.nextInt(), convo.getAvRTT()), conversationId);
+                System.out.println(parent.getSelfLink()+" - asInitiatorDirect success because [ALREADY CONNECTED]");
                 return true;
             } else {
-                ReceivedP2LMessage peerLinkMessage = convo.answerExpect(verifyNonce);
-                convo.close();
-
-                if (peerLinkMessage.getPayloadLength() > 0) {
-                    parent.graduateToEstablishedConnection(connectionFromMessage(peerLinkMessage, peerLinkMessage.sender, convo.getAvRTT()), conversationId);
-                    System.out.println(parent.getSelfLink()+" - asInitiatorDirect success because received remote self link");
-                    return true;
-                } else
-                    return false;
+                parent.graduateToEstablishedConnection(connectionFromMessage(peerLinkMessage, peerLinkMessage.sender, convo.getAvRTT()), conversationId);
+                System.out.println(parent.getSelfLink()+" - asInitiatorDirect success because received remote self link");
+                return true;
             }
         } catch (TimeoutException | IOException e) {
             return false;
@@ -68,32 +56,22 @@ public class DirectConnectionProtocol {
 
     public static void asAnswerer(P2LNodeInternal parent, P2LConversation convo, ReceivedP2LMessage initialRequestMessage) throws IOException {
         System.out.println(parent.getSelfLink()+" - DirectConnectionProtocol.asAnswerer");
+        initialRequestMessage.nextByte();//skips the single byte up front
         P2LConnection newPeerConnection = connectionFromMessage(initialRequestMessage, initialRequestMessage.sender, -1);
         System.out.println(parent.getSelfLink()+" - newPeerConnection = " + newPeerConnection);
 
         if (parent.connectionLimitReached()) {
-            convo.answerClose(new byte[0]);
+            convo.answerClose(convo.encode(REFUSED));
         } else if (parent.isConnectedTo(newPeerConnection.link)) {
-            convo.answerClose(BitHelper.getBytes(P2LMessage.CUSTOM_RAW_SIZE_LIMIT));
+            convo.answerClose(convo.encode(ALREADY_CONNECTED, P2LMessage.CUSTOM_RAW_SIZE_LIMIT));
         } else {
-            // send a nonce to other peer
-            //   if they are receiving correctly on their port they should read the nonce and be able to send it back
-            //   (if they are not able to do this, then they may have spoofed their sender address and tried to fill this nodes peer list - or they are )
-            byte[] nonce = new byte[16]; //must be != 4
-            secureRandom.nextBytes(nonce);
-
-            byte[] verifyNonce = convo.answerExpectData(nonce);
-
-            if (Arrays.equals(nonce, verifyNonce)) {
-                parent.graduateToEstablishedConnection(newPeerConnection, initialRequestMessage.header.getConversationId());
-                convo.answerClose(linkToMessage(parent.getSelfLink(), convo));
-            } else
-                convo.answerClose(new byte[0]);
+            parent.graduateToEstablishedConnection(newPeerConnection, initialRequestMessage.header.getConversationId());
+            convo.answerClose(linkToMessage(parent.getSelfLink(), convo));
         }
     }
 
     private static MessageEncoder linkToMessage(P2Link selfLink, P2LConversation convo) {
-        return convo.encode(selfLink.toBytes(), P2LMessage.CUSTOM_RAW_SIZE_LIMIT);
+        return convo.encode(NEW_CONNECTION_ESTABLISHED, selfLink.toBytes(), P2LMessage.CUSTOM_RAW_SIZE_LIMIT);
     }
 
     private static P2LConnection connectionFromMessage(P2LMessage mx, InetSocketAddress address, int avRTT) {
