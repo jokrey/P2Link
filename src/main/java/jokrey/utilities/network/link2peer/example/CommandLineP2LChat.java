e26e33ccec4ff2feacf576b3dafa4d173b4f841e
josh <6jkruege@informatik.uni-hamburg.de>
2022-09-29 00:37:30 +0200

    improve structure

--- src/main/java/jokrey/utilities/network/link2peer/example/CommandLineP2LChat.java
+++ src/main/java/jokrey/utilities/network/link2peer/example/CommandLineP2LChat.java
@@ -24,36 +24,14 @@ public class CommandLineP2LChat {
         System.out.println("NOTE: If you are trying to connect to a peer via ipv4 that is behind the same router,\n" +
                 "   via its public-ip/gateway-ip(external nat-address, as for example seen by a rendezvous server behind that router access via that public ip),\n" +
                 "   then be aware this ONLY works if the router supports NAT-Loopback/NAT-Hairpinning (some do not in 2020)....");
+        P2Link selfLink = userPromptForSelfLink(arguments);
 
-        Scanner userIn = new Scanner(System.in);
+        P2LNode node = P2LNode.create(selfLink);
 
-        String rawInput;
-        if(arguments.length > 0) {
-            rawInput = arguments[0];
-        } else {
-            System.out.println("First argument should be the port on which this node should listen for incoming messages\n   preferred however is a full socket address (<ip/dns>:<port>) on which other peers can reach this node");
-            System.out.print("Enter own link (type \"format\" to see a description of the link format):  \n");
-            while((rawInput = userIn.nextLine()).equalsIgnoreCase("format")) {
-                System.out.println("Link Format:\n" +
-                        "  direct link:\n" +
-                        "     <ip/dns>:<port>\n" +
-                        "  relayed link [NO USE HERE - CAN BE USED TO CONNECT]:\n" +
-                        "     <name>[<direct link of relay server>]\n" +
-                        "  local link:\n" +
-                        "     <name>[local=<port>]");
-                System.out.print("Enter own link (type \"format\" to see a description of the link format):  \n");
-            }
-        }
-        String rawLink = rawInput;
-
-        System.out.println("Given = " + rawLink);
-
-        P2LNode node = P2LNode.create(P2Link.from(rawLink));
-
-        node.addMessageListener(message -> System.out.println(rawLink+" received message(from "+message.sender+"):\n"+message.asString()));
-        node.addBroadcastListener(message -> System.out.println(rawLink+" received broadcast(from "+message.source+"):\n"+message.asString()));
-        node.addConnectionEstablishedListener((newAddress, cId) -> System.out.println(rawLink+" established a new connection to "+newAddress.link));
-        node.addConnectionDroppedListener(disconnected -> System.out.println(rawLink+" connection to "+disconnected.link +" disconnected"));
+        node.addMessageListener(message -> System.out.println(selfLink+" received message(from "+message.sender+"):\n"+message.asString()));
+        node.addBroadcastListener(message -> System.out.println(selfLink+" received broadcast(from "+message.source+"):\n"+message.asString()));
+        node.addConnectionEstablishedListener((newAddress, cId) -> System.out.println(selfLink+" established a new connection to "+newAddress.link));
+        node.addConnectionDroppedListener(disconnected -> System.out.println(selfLink+" connection to "+disconnected.link +" disconnected"));
 
         CommandLoop loop = new CommandLoop();
         loop.addCommand("connectTo", "Connects to peer at link(args[0]) of the form [ip/dns]:[port]", Argument.with(String.class), args -> {
@@ -103,7 +81,7 @@ public class CommandLineP2LChat {
             P2Link to = P2Link.from(args[0].getRaw()); //unresolved, maybe
             try {
                 P2LFuture<Boolean> successFut = node.sendMessageWithReceipt(to, P2LMessage.with(0, args[1].getRaw().getBytes(StandardCharsets.UTF_8)));
-                Boolean success = successFut.getOrNull(P2LHeuristics.DEFAULT_PROTOCOL_ANSWER_RECEIVE_TIMEOUT * 2);
+                Boolean success = successFut.getOrNull(P2LHeuristics.DEFAULT_PROTOCOL_ANSWER_RECEIVE_TIMEOUT * 2L);
                 System.out.println(success!=null&&success?"successfully send":"no response (peer did not receive)");
             } catch (IOException e) {
                 System.err.println("error sending message "+e.getMessage());
@@ -113,5 +91,29 @@ public class CommandLineP2LChat {
         loop.addCommand("printDebugInformation", "Prints debug information", Argument.noargs(), args -> node.printDebugInformation(), "debug");
 
         loop.run();
+    }
+
+    public static P2Link userPromptForSelfLink(String[] arguments) {
+        Scanner userIn = new Scanner(System.in);
+
+        String rawInput;
+        if(arguments.length > 0) {
+            rawInput = arguments[0];
+        } else {
+            System.out.println("First argument should be the port on which this node should listen for incoming messages\n   preferred however is a full socket address (<ip/dns>:<port>) on which other peers can reach this node");
+            System.out.print("Enter own link (type \"format\" to see a description of the link format):  \n");
+            while((rawInput = userIn.nextLine()).equalsIgnoreCase("format")) {
+                System.out.println("Link Format:\n" +
+                        "  direct link:\n" +
+                        "     <ip/dns>:<port>\n" +
+                        "  relayed link [NO USE HERE - CAN BE USED TO CONNECT]:\n" +
+                        "     <name>[<direct link of relay server>]\n" +
+                        "  local link:\n" +
+                        "     <name>[local=<port>]");
+                System.out.print("Enter own link (type \"format\" to see a description of the link format):  \n");
+            }
+        }
+        System.out.println("Given = " + rawInput);
+        return P2Link.from(rawInput);
     }
 }
